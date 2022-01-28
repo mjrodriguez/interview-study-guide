5e8b6346da471198a7a035665cc5eb800c58bb20
Martin Rodriguez <mrodrig6@ucsc.edu>
2022-01-27 23:02:40 -0800

    stack and queue

--- python-dsa-code/stack.py
+++ python-dsa-code/stack.py
@@ -9,4 +9,40 @@ class stack:
 		self.top = new_node
 		self.height = 1
 
+	def print(self):
+		temp = self.top
+		while temp is not None:
+			print(temp.value)
+			temp = temp.next	
+		print("-----")
 	
+	def push(self, value):
+		new_node = Node(value)
+		if self.height == 0:
+			self.top = new_node
+		else:
+			new_node.next = self.top
+			self.top = new_node
+		
+		self.height += 1
+		return True
+	
+	def pop(self):
+		if self.height == 0:
+			return None
+
+		temp = self.top
+		self.top = temp.next
+		temp.next = None
+		
+		self.height -= 1
+		return temp
+
+
+s = stack(4)
+s.push(3)
+s.print()
+s.pop()
+s.pop()
+s.pop()
+s.print()
